e3be10b48ca430ca4198632cc245c90c6128bc9b
bbuilds <hello@brandenbuilds.com>
2024-04-04 21:30:39 -0500

    fix: :white_check_mark: update vitepath aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
@@ -5,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@/*": path.resolve(__dirname, "./src/*"),
+    },
   },
 });
