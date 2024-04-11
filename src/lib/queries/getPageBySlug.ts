c12713c16ff952f714670b0847deb4d60fe524dd
bbuilds <hello@brandenbuilds.com>
2024-04-10 20:01:19 -0500

    feat: :sparkles: setup graphql fetch client

---
+++ src/lib/queries/getPageBySlug.ts
@@ -0,0 +1,49 @@
+import { fetchGraphQL } from "@/lib/api";
+import { Page } from "@/lib/types";
+
+/**
+ * Fetch a page by slug.
+ */
+export default async function getPageBySlug(slug: string) {
+  const query = `
+    query GetPageBySlug($slug: ID = "URI") {
+      page(idType: URI, id: $slug) {
+        databaseId
+        date
+        modified
+        content(format: RENDERED)
+        title(format: RENDERED)
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+            mediaDetails {
+                height
+                width
+            }
+          }
+        }
+        author {
+          node {
+            name
+            avatar {
+              url
+            }
+          }
+        }
+        seo {
+          metaDesc
+          title
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    slug: slug,
+  };
+
+  const response = await fetchGraphQL(query, variables);
+
+  return response.data.page as Page;
+}
