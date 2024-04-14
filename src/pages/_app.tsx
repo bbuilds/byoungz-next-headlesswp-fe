82c7e3ae0251b9cdaaa2dddf46c8b2da50fbc86f
bbuilds <hello@brandenbuilds.com>
2024-04-14 17:16:31 -0500

    feat: :wheelchair: add dev a11y report

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import * as React from "react";
 import "@/src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { a11yReport } from "@/src/lib/utils";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,3 +11,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </ThemeProvider>
   );
 }
+
+//Adds report to console in dev mode if there are accessibility issues
+void a11yReport(React);
