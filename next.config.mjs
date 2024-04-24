58d259af1871b804562105d413885f20732ffc0c
bbuilds <hello@brandenbuilds.com>
2024-04-23 21:51:50 -0500

    feat: :sparkles: add remote images setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: process.env.NEXT_PUBLIC_HOSTNAME,
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_HOSTNAME,
+        port: "",
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
