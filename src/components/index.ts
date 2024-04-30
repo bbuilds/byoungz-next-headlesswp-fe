7b0b762988522f5ba7d0c59bb9cdb7107123124a
bbuilds <hello@brandenbuilds.com>
2024-04-29 21:17:34 -0500

    feat: :sparkles: create postlist component

--- src/components/index.ts
+++ src/components/index.ts
@@ -12,4 +12,5 @@ export * from "./AboutSection";
 export * from "./RichText";
 export * from "./SectionHeader";
 export * from "./InstagramFeed";
+export * from "./PostList";
 export * from "./CardArticle";
