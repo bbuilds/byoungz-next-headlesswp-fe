ec2bd2a3b52fbdb47cd4df6b0ce8e8e5fba679b0
bbuilds <hello@brandenbuilds.com>
2024-05-06 20:13:57 -0500

    fix: :test_tube: Breadcrumbs unit test

--- src/components/Breadcrumbs/Breadcrumbs.test.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.test.jsx
@@ -3,15 +3,26 @@ import { render } from "@testing-library/react";
 import { Breadcrumbs } from "@/src/components";
 import { axe } from "vitest-axe";
 
+const breadcrumbs = [
+  {
+    text: "Home",
+    url: "http://byoungzapi.local/",
+  },
+  {
+    text: "Dope Swimming Spots in Puerto Rico",
+    url: "http://byoungzapi.local/swimming-spots-in-puerto-rico/",
+  },
+];
+
 describe("Breadcrumbs", () => {
   test("renders a Breadcrumbs component", () => {
-    const { getByText } = render(<Breadcrumbs>Hello world!</Breadcrumbs>);
+    const { getByText } = render(<Breadcrumbs links={breadcrumbs} />);
 
-    expect(getByText("Hello world!")).toBeTruthy();
+    expect(getByText("Dope Swimming Spots in Puerto Rico")).toBeTruthy();
   });
 
   test("is accessible", async () => {
-    const { container } = render(<Breadcrumbs>Hello world!</Breadcrumbs>);
+    const { container } = render(<Breadcrumbs links={breadcrumbs} />);
     expect(await axe(container)).toHaveNoViolations();
   });
 });
