8357ad12ce145286e94d8a8a72619272a25555db
bbuilds <hello@brandenbuilds.com>
2024-10-27 16:34:49 -0500

    feat: :sparkles: add view all blog posts to home

--- src/components/RecentPostsSection/RecentPostsSection.tsx
+++ src/components/RecentPostsSection/RecentPostsSection.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { SectionHeader, PostList } from "@/src/components";
+import { SectionHeader, PostList, SmartLink } from "@/src/components";
 import type { Post } from "@/src/lib/types";
 
 export type RecentPostsSectionProps = {
@@ -13,6 +13,11 @@ export function RecentPostsSection({ title, posts }: RecentPostsSectionProps) {
       <div className="container mx-auto">
         <SectionHeader text={title} />
         <PostList posts={posts} displayAmount={3} useLoadMore={false} />
+        <div className="mt-10 flex justify-center">
+          <SmartLink href="/blog" classNames="anchor-hover text-base">
+            View All Blog Posts
+          </SmartLink>
+        </div>
       </div>
     </section>
   );
