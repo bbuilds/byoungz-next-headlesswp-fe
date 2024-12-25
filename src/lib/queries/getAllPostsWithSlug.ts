69eab2f3861f6e2b54b5a096fa9cc0a5df60993d
bbuilds <hello@brandenbuilds.com>
2024-12-25 17:50:48 -0500

    fix: :bug: paginate static posts builder

--- src/lib/queries/getAllPostsWithSlug.ts
+++ src/lib/queries/getAllPostsWithSlug.ts
@@ -2,17 +2,35 @@ import { fetchGraphQL } from "@/src/lib/api";
 import { RootQueryToPostConnection } from "@/src/lib/types";
 
 export async function getAllPostsWithSlug() {
-  const query = `
-    query getAllPostsWithSlug {
-      posts {
-        nodes {
-          slug
+  async function fetchPosts(after: string | null = null) {
+    const query = `
+      query getAllPostsWithSlug($after: String) {
+        posts(first: 100, after: $after, where: {status: PUBLISH}) {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          nodes {
+            slug
+          }
         }
       }
-    }
-  `;
+    `;
 
-  const response = await fetchGraphQL(query);
+    const response = await fetchGraphQL(query, { after });
+    return response.data.posts;
+  }
 
-  return response.data.posts as RootQueryToPostConnection;
+  let allPosts: any = { nodes: [] };
+  let hasNextPage = true;
+  let endCursor = null;
+
+  while (hasNextPage) {
+    const response = await fetchPosts(endCursor);
+    allPosts.nodes = [...allPosts.nodes, ...response.nodes];
+    hasNextPage = response.pageInfo.hasNextPage;
+    endCursor = response.pageInfo.endCursor;
+  }
+
+  return allPosts as RootQueryToPostConnection;
 }
